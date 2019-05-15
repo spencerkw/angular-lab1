d4afa6653d659b0c6f97e1e5a5e413fd476458da
Spencer K-W <keashlywarters@gmail.com>
2019-05-15 16:02:21 -0400

    part 1 of the lab done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-lab1';
+  todoList: Todo[] = [
+    {
+      task: "Walk the dog",
+      completed: false
+    },
+    {
+      task: "Go to the store",
+      completed: true
+    },
+    {
+      task: "Get gas",
+      completed: false
+    },
+    {
+      task: "Wash the car",
+      completed: false
+    }
+  ];
+  constructor() {}
+  
 }
